7739e55d471773bbc38cb4830d3d9f3a9fdd20e9
BrendaEduarda <contatobrendadiniz.s2@gmail.com>
2023-11-09 22:16:37 -0300

    Commit Inicial

---
+++ voting_management/src/test/java/com/brendasoares/voting_management/VotingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendasoares.voting_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
